108387401d2185e0fd9a539ff5f6bb9245a34872
Abir Bouhriz Daidj <bouhrizabir2015@gmail.com>
2021-04-27 16:13:54 +0100

    50 30 20 strategy

---
+++ tooEasy/50_30_20_strategy.js
@@ -0,0 +1,9 @@
+function fiftyThirtyTwenty(ati) {
+var	needs= (ati/100)*50;
+var wants= (ati/100)*30;
+var savings= (ati/100)*20;
+
+return ["Needs: " + needs + ", " + 
+    "Wants: " + wants + ", " +
+    "Savings: " + savings ];
+}
